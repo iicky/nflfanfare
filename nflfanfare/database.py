fcd6bcca0b261ea066501133801848bf4e18bb01
Mickey Scherrer <scherrer@Selene.local>
2015-12-18 17:58:29 -0600

    Updated table declarations with __init__

--- nflfanfare/database.py
+++ nflfanfare/database.py
@@ -1,11 +1,8 @@
 import secrets as sec
 import sqlalchemy as sql
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
-
-Base = declarative_base()
-
 
 class DB:
     ''' Database handling
@@ -18,11 +15,11 @@ class DB:
             sec.mysqluser, sec.mysqlpwd, sec.mysqlhost)
         self.engine = sql.create_engine(self.enginestr)
 
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
+        self.session = scoped_session(sessionmaker(bind=self.engine))
+        self.Base = declarative_base()
+        self.Base.query = self.session.query_property()
 
-
-class Schedule(Base):
+class Schedule(DB().Base):
     __tablename__ = 'schedule'
 
     gameid = Column(String, primary_key=True)
@@ -40,15 +37,50 @@ class Schedule(Base):
     vegasline = Column(String)
     overunder = Column(String)
 
-class TeamHashtags(Base):
+    def __init__(self, gameid=None,
+                 eid=None,
+                 week=None,
+                 seasontype=None,
+                 hometeam=None,
+                 awayteam=None,
+                 starttime=None,
+                 endtime=None,
+                 stadium=None,
+                 weather=None,
+                 wontoss=None,
+                 attendance=None,
+                 vegasline=None,
+                 overunder=None):
+        self.gameid = gameid
+        self.eid = eid
+        self.week = week
+        self.seasontype = seasontype
+        self.hometeam = hometeam
+        self.awayteam = awayteam
+        self.starttime = starttime
+        self.endtime = endtime
+        self.stadium = stadium
+        self.weather = weather
+        self.wontoss = attendance
+        self.vegasline = vegasline
+        self.overunder = overunder
+
+
+class TeamHashtags(DB().Base):
     __tablename__ = 'teamhashtags'
-    
+
     hashtag = Column(String, primary_key=True)
     teamid = Column(String)
-    
-class Tweets(Base):
+
+    def __init__(self, hashtag=None,
+                 teamid=None):
+        self.hashtag = hashtag
+        self.teamid = teamid
+
+
+class Tweets(DB().Base):
     __tablename__ = 'tweets'
-    
+
     tweetid = Column(String, primary_key=True)
     teamid = Column(String)
     userid = Column(String)
@@ -67,10 +99,48 @@ class Tweets(Base):
     sent_neg = Column(String)
     sent_neu = Column(String)
     sent_compound = Column(String)
-    
-class Teams(Base):
+
+    def __init__(self, tweetid=None,
+                 teamid=None,
+                 userid=None,
+                 username=None,
+                 realname=None,
+                 userlocation=None,
+                 usertimezone=None,
+                 userprofileimg=None,
+                 tweettext=None,
+                 language=None,
+                 hashtags=None,
+                 usermentions=None,
+                 postedtime=None,
+                 collectedtime=None,
+                 sent_pos=None,
+                 sent_neg=None,
+                 sent_neu=None,
+                 sent_compound=None):
+        self.tweetid = tweetid
+        self.teamid = teamid
+        self.userid = userid
+        self.username = username
+        self.realname = realname
+        self.userlocation = userlocation
+        self.usertimezone = usertimezone
+        self.userprofileimg = userprofileimg
+        self.tweettext = tweettext
+        self.language = language
+        self.hashtags = hashtags
+        self.usermentions = usermentions
+        self.postedtime = postedtime
+        self.collectedtime = collectedtime
+        self.sent_pos = sent_pos
+        self.sent_neg = sent_neg
+        self.sent_neu = sent_neu
+        self.sent_compound = sent_compound
+
+
+class Teams(DB().Base):
     __tablename__ = 'teams'
-    
+
     teamid = Column(String, primary_key=True)
     teamcity = Column(String)
     teamname = Column(String)
@@ -79,3 +149,20 @@ class Teams(Base):
     homecolor = Column(String)
     awaycolor = Column(String)
     pfrid = Column(String)
+
+    def __init__(self, teamid=None,
+                 teamcity=None,
+                 teamname=None,
+                 conference=None,
+                 division=None,
+                 homecolor=None,
+                 awaycolor=None,
+                 pfrid=None):
+        self.teamid = teamid
+        self.teamcity = teamcity
+        self.teamname = teamname
+        self.conference = conference
+        self.division = division
+        self.homecolor = homecolor
+        self.awaycolor = awaycolor
+        self.pfrid = pfrid
