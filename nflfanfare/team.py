2eb380c495b88bc213f8ad4c12f46dbd7e1b56b1
Mickey Scherrer <scherrer@Selene.local>
2015-12-18 17:04:16 -0600

    team.py updated to SQLAlchemy

--- nflfanfare/team.py
+++ nflfanfare/team.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql.expression import func
+
 import nflfanfare as ff
 
 
@@ -6,63 +8,45 @@ class Team:
     '''
 
     def __init__(self):
-    	pass
+        pass
 
     def team_ids(self):
         ''' Returns NFL Team IDs as a list
         '''
-        query = """SELECT `teamid` FROM `teams`;"""
-        result = list(ff.db.engine.execute(query))
+        result = ff.db.session.query(ff.db.teams.teamid).all()
         return [r[0] for r in result]
 
     def team_hashtags(self, teamid):
         ''' Returns all hashtags for NFL team
         '''
-        query = """SELECT `hashtag` FROM `teamhashtags`
-					WHERE `teamid`='%s';
-				""" % (teamid)
-        result = list(ff.db.engine.execute(query))
+        result = ff.db.session.query(
+            ff.db.teamhashtags.hashtag).filter_by(teamid=teamid).all()
         return [r[0] for r in result]
 
     def rand_hashtag(self, teamid):
         ''' Returns random hashtag for NFL team
         '''
-        query = """SELECT `hashtag` FROM `teamhashtags`
-						WHERE `teamid`='%s'
-						ORDER BY RAND() LIMIT 1;
-			""" % (teamid)
-        return list(ff.db.engine.execute(query))[0][0]
-
-    def team_hashtag(self, hashtag):
-        ''' Returns the teamid for a hashtag
-        '''
-        query = """ SELECT `teamid` FROM `teamhashtags`
-					WHERE `hashtag`='%s'
-					LIMIT 1;
-				""" % (hashtag)
-        return list(ff.db.engine.execute(query))[0][0]
+        result = ff.db.session.query(ff.db.teamhashtags.hashtag).filter_by(
+            teamid=teamid).order_by(func.rand()).first()
+        return result[0]
 
     def teamid_from_name(self, teamname):
         ''' Returns the team id from team full name
         '''
-        query = """ SELECT `teamid` FROM `teams`
-					WHERE CONCAT(`teamcity`, ' ', `teamname`) = '%s';
-				""" % teamname
-        return list(ff.db.engine.execute(query))[0][0]
+        result = ff.db.session.query(ff.db.teams.teamid).filter(func.concat(
+            ff.db.teams.teamcity, ' ', ff.db.teams.teamname) == teamname).one()
+        return result[0]
 
     def teamid_from_hashtag(self, hashtag):
         ''' Returns the teamid for a hashtag
         '''
-        query = """ SELECT `teamid` FROM `teamhashtags`
-					WHERE `hashtag`='%s'
-					LIMIT 1;
-				""" % (hashtag)
-        return list(ff.db.engine.execute(query))[0][0]
+        result = ff.db.session.query(
+            ff.db.teamhashtags.teamid).filter_by(hashtag=hashtag).one()
+        return result[0]
 
     def pfrid_from_teamid(self, teamid):
-		''' Returns the PFR ID from team id
-		'''
-		query = """ SELECT `PFRid` FROM `teams`
-					WHERE `teamid` = '%s';
-				""" % teamid
-		return list(ff.db.engine.execute(query))[0][0]
+        ''' Returns the PFR ID from team id
+        '''
+        result = ff.db.session.query(
+            ff.db.teams.pfrid).filter_by(teamid=teamid).one()
+        return result[0]
