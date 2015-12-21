9ee02cf8a442fd125be85194b257646a18f7e502
Mickey Scherrer <scherrer@iicky.com>
2015-12-21 15:40:51 -0600

    Added game_tweet_counts function

---
+++ nflfanfare/statistics.py
@@ -0,0 +1,15 @@
+import nflfanfare as ff
+
+class Statistics:
+    ''' Schedule class
+    '''
+
+    def __init__(self):
+        pass
+
+    def game_tweet_counts(self):
+    	''' Returns pandas dataframe of game info and tweet counts
+    	'''
+    	df = ff.sched.all_games()
+    	df['tweetcount'] = df.gameid.apply(ff.sched.tweet_count)
+    	return df
