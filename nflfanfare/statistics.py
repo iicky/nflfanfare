e19718640d5bdc7c3df87ca6b0a53e989c408d7a
Mickey Scherrer <scherrer@iicky.com>
2015-12-22 13:37:51 -0600

    Added teams_list function

--- nflfanfare/statistics.py
+++ nflfanfare/statistics.py
@@ -11,5 +11,11 @@ class Statistics:
     	''' Returns pandas dataframe of game info and tweet counts
     	'''
     	df = ff.sched.all_games()
-    	df['tweetcount'] = df.gameid.apply(ff.sched.tweet_count)
+    	#df['tweetcount'] = df.gameid.apply(ff.sched.tweet_count)
+    	return df
+
+    def teams_list(self):
+    	''' Returns a list of team info
+    	'''
+    	df = ff.team.teams()
     	return df
