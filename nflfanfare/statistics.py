ad21b69b209cb8106907bf7332f8d7183f34226b
Mickey Scherrer <scherrer@iicky.com>
2015-12-23 17:59:10 -0600

    Subsetted data frame in game_tweet_counts function

--- nflfanfare/statistics.py
+++ nflfanfare/statistics.py
@@ -1,5 +1,6 @@
 import nflfanfare as ff
 
+
 class Statistics:
     ''' Schedule class
     '''
@@ -8,13 +9,15 @@ class Statistics:
         pass
 
     def game_tweet_counts(self):
-    	''' Returns pandas dataframe of game info and tweet counts
-    	'''
-    	df = ff.sched.all_tweet_counts()
-    	return df
+        ''' Returns pandas dataframe of game info and tweet counts
+        '''
+        df = ff.sched.all_tweet_counts()
+        df = df[["gameid", "week", "seasontype", "hometeam",
+                 "awayteam", "starttime", "endtime", "tweetcount"]]
+        return df
 
     def teams_list(self):
-    	''' Returns a list of team info
-    	'''
-    	df = ff.team.teams()
-    	return df
+        ''' Returns a list of team info
+        '''
+        df = ff.team.teams()
+        return df
