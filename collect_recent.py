3b4805320af6cbeca57f6ed5aba981c4e5da3568
Mickey Scherrer <scherrer@iicky.com>
2015-12-24 16:07:05 -0600

    Helper script to collect tweets for recent games
    Run as cronjob every Sunday

---
+++ collect_recent.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import nflfanfare as ff
+
+games = ff.sched.all_games()
+
+for g in games.gameid:
+	status = ff.sched.game_status(g)
+	if status == "recent":
+		if ff.sched.tweet_count(g) < 5000:
+			try:
+				ff.col.collect_recent(g)
+			except:
+				pass
+		
