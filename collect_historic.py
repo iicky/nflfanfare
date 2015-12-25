9730fe6b63b873b00b7f3e5b5df9667867852f03
Mickey Scherrer <scherrer@iicky.com>
2015-12-25 10:55:41 -0600

    Fixed recent game bug

--- collect_historic.py
+++ collect_historic.py
@@ -6,7 +6,7 @@ games = ff.sched.all_games()
 
 for g in games.gameid:
 	status = ff.sched.game_status(g)
-	if status == "recent":
+	if status == "historic":
 		if ff.sched.tweet_count(g) < 10000:
 			try:
 				ff.col.collect_historic(g, verbose=True)
