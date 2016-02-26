b9592e13d2fbff7233c25a43feab6b4c8752fbd6
Mickey Scherrer <scherrer@iicky.com>
2016-02-26 17:22:50 -0500

    PEP8 Formatting

--- nflfanfare/database.py
+++ nflfanfare/database.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 
 from nflfanfare import secrets as sec
 
+
 class DB:
     ''' Database handling
     '''
@@ -10,10 +11,13 @@ class DB:
         ''' Creates database engine
         '''
         self.client = MongoClient(
-            'mongodb://%s:%s@%s:27017/NFL' % (sec.mongouser, sec.mongopwd, sec.mongohost))
+            'mongodb://%s:%s@%s:27017/NFL' % (sec.mongouser,
+                                              sec.mongopwd,
+                                              sec.mongohost))
         self.db = self.client.NFL
 
         self.teams = self.db.teams
         self.games = self.db.games
+        self.pfrplays = self.db.pfrplays
         self.tweets = self.db.tweets
         self.teamtweets = self.db.teamtweets
